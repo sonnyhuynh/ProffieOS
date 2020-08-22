7d9d8d005de67a3f93ccfc53d8d4f0a7101b3d46
Sonny Huynh <sonny@sonnyhuynh.com>
2020-08-22 16:03:48 -0400

    Add dual phase and no blade styles

--- blades/son_blades.h
+++ blades/son_blades.h
@@ -66,3 +66,25 @@ StyleAllocator vaderStyle =
 StylePtr<Layers<Mix<SwingSpeed<400>,AudioFlicker<RotateColorsX<Variation,Red>,RotateColorsX<Variation,Rgb<128,0,0>>>,Mix<SwingSpeed<600>,RotateColorsX<Variation,Red>,RotateColorsX<Variation,Tomato>>>,TransitionEffectL<TrConcat<TrInstant,BrownNoiseFlickerL<AlphaL<White,Int<16000>>,Int<50>>,TrSmoothFade<600>>,EFFECT_LOCKUP_END>,ResponsiveLockupL<Strobe<White,BrownNoiseFlicker<White,Blue,300>,50,1>,TrConcat<TrInstant,White,TrFade<200>>,TrFade<400>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>,Scale<SwingSpeed<100>,Int<10000>,Int<14000>>>,ResponsiveLightningBlockL<Strobe<White,AudioFlicker<White,Blue>,50,1>,TrConcat<TrInstant,AlphaL<White,Bump<Int<12000>,Int<18000>>>,TrFade<200>>,TrConcat<TrInstant,HumpFlickerL<AlphaL<White,Int<16000>>,30>,TrSmoothFade<600>>>,ResponsiveBlastL<White,Int<400>,Scale<SwingSpeed<200>,Int<100>,Int<400>>,Int<400>>,ResponsiveClashL<White,TrInstant,TrFade<400>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>>,ResponsiveStabL<Orange,TrWipeIn<600>,TrWipe<600>>,ResponsiveDragL<BrownNoiseFlickerL<White,Int<300>>,TrWipeIn<400>,TrFade<400>>,ResponsiveMeltL<Mix<TwistAngle<>,OrangeRed,Orange>,TrWipeIn<600>,TrSmoothFade<600>>,InOutTrL<TrWipe<300>,TrWipeIn<500>,Black>,TransitionEffectL<TrConcat<TrDelay<1500>,Black,TrFade<1000>,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_BOOT>,TransitionEffectL<TrConcat<TrInstant,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_NEWFONT>>>()
 ;
 
+// fully responsive audioflicker color swing w/ font battery monitor, responsive ignition, 1s retraction dark orange
+// dual phase
+//    audioflicker orange
+//    Rgb<48,0,105> (purple) for clash, lockup, blast
+// -- color change boing
+//    audioflicker purple
+//    orange for clash, lockup, blast
+StyleAllocator sonStyle =
+StylePtr<Layers<Mix<SwingSpeed<400>,ColorChange<TrBoing<300,3>,AudioFlicker<DarkOrange,Rgb<128,34,0>>,AudioFlicker<Rgb<95,0,210>,Rgb<48,0,105>>>,Mix<SwingSpeed<600>,ColorChange<TrBoing<300,3>,DarkOrange,Rgb<95,0,210>>,ColorChange<TrBoing<300,3>,Orange,Magenta>>>,TransitionEffectL<TrConcat<TrInstant,BrownNoiseFlickerL<AlphaL<ColorChange<TrInstant,Rgb<48,0,105>,Orange>,Int<16000>>,Int<50>>,TrSmoothFade<600>>,EFFECT_LOCKUP_END>,ResponsiveLockupL<Strobe<ColorChange<TrInstant,Rgb<48,0,105>,Orange>,BrownNoiseFlicker<ColorChange<TrInstant,Rgb<48,0,105>,Orange>,White,300>,50,1>,TrConcat<TrInstant,ColorChange<TrInstant,Rgb<48,0,105>,Orange>,TrFade<200>>,TrFade<400>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>,Scale<SwingSpeed<100>,Int<10000>,Int<14000>>>,ResponsiveLightningBlockL<Strobe<White,AudioFlicker<White,Blue>,50,1>,TrConcat<TrInstant,AlphaL<White,Bump<Int<12000>,Int<18000>>>,TrFade<200>>,TrConcat<TrInstant,HumpFlickerL<AlphaL<White,Int<16000>>,30>,TrSmoothFade<600>>>,ResponsiveBlastL<ColorChange<TrInstant,Rgb<48,0,105>,Orange>,Int<400>,Scale<SwingSpeed<200>,Int<100>,Int<400>>>,ResponsiveClashL<ColorChange<TrInstant,Rgb<48,0,105>,Orange>,TrInstant,TrFade<400>>,ResponsiveStabL<Red>,ResponsiveDragL<BrownNoiseFlickerL<White,Int<300>>,TrWipeIn<400>,TrFade<400>>,ResponsiveMeltL<Mix<TwistAngle<>,Red,Orange>,TrWipeIn<600>,TrSmoothFade<600>>,InOutTrL<TrWipeX<Scale<BladeAngle<>,Int<500>,Int<200>>>,TrWipeIn<1000>>,TransitionEffectL<TrConcat<TrDelay<1500>,Black,TrFade<1000>,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_BOOT>,TransitionEffectL<TrConcat<TrInstant,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_NEWFONT>>>()
+;
+
+// no off behavior
+// on behavior
+//   fast turbine, green, color change on
+//   continuous run time
+//   red flash for blast, clash
+//   red flashing for lockup, lightning
+//   white flashing for lightning
+StyleAllocator nobladeStyle =
+StylePtr<Layers<ColorCycle<Red,25,100,RotateColorsX<Variation,Green>,25,300,1000>,TransitionEffectL<TrConcat<TrInstant,Red,TrFade<400>>,EFFECT_BLAST>,LockupTrL<AudioFlickerL<Red>,TrInstant,TrFade<500>,SaberBase::LOCKUP_NORMAL>,LockupTrL<AudioFlickerL<White>,TrInstant,TrFade<500>,SaberBase::LOCKUP_LIGHTNING_BLOCK>,TransitionEffectL<TrConcat<TrInstant,Red,TrFade<400>>,EFFECT_CLASH>,InOutTrL<TrInstant,TrColorCycle<1000,300,0>>>>()
+;
+
