ee77a176b622f47e3c9869cec13cd5821a92ffe1
Sonny Huynh <sonny@sonnyhuynh.com>
2020-08-22 16:17:47 -0400

    Add "noblade" save file

--- config/son_proffieboard_config.h
+++ config/son_proffieboard_config.h
@@ -100,7 +100,8 @@ BladeConfig blades[] = {
 //    WS281XBladePtr<21, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
 // npxl
     WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    CONFIGARRAY(noBladePresets)
+    CONFIGARRAY(noBladePresets),
+    "noblade",
   },
 };
 #endif
