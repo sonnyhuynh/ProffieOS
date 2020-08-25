3e418b60a97d0045e7e5f49c540ba95ec109b3ad
Sonny Huynh <sonny@sonnyhuynh.com>
2020-08-25 19:54:49 -0400

    Set preset directory

--- config/son_proffieboard_config.h
+++ config/son_proffieboard_config.h
@@ -74,7 +74,8 @@ BladeConfig blades[] = {
 //    WS281XBladePtr<21, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
 // npxl
     WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    CONFIGARRAY(presets)
+    CONFIGARRAY(presets),
+    "CONFIG/blade"
   },
 };
 #endif
