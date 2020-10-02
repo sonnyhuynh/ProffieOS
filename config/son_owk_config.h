46a875948114476313ebdf41c6351b36f33f1e59
Sonny Huynh <sonny@sonnyhuynh.com>
2020-10-01 21:24:37 -0400

    Add owk config

---
+++ config/son_owk_config.h
@@ -0,0 +1,122 @@
+#ifdef CONFIG_TOP
+#include "proffieboard_v2_config.h"
+
+#define NUM_BLADES 2
+#define NUM_BUTTONS 1
+#define VOLUME 100
+#define MAX_VOLUME 1500
+const unsigned int maxLedsPerStrip = 144;
+#define CLASH_THRESHOLD_G 2.9
+#define ENABLE_AUDIO
+#define ENABLE_MOTION
+#define ENABLE_WS2811
+#define ENABLE_SD
+#define SAVE_PRESET
+// this creates the preset.ini file
+#define SAVE_COLOR_CHANGE
+
+// npxl
+#define SHARED_POWER_PINS
+
+// blade detect
+#define BLADE_DETECT_PIN blade3Pin
+
+// blade id
+#define ENABLE_POWER_FOR_ID PowerPINS<bladePowerPin2,bladePowerPin3>
+
+#define SON_SWING_ON
+#define SON_TWIST_ON
+#define SON_TWIST_OFF
+#define FETT263_BATTLE_MODE
+
+// dual phase styles
+#define COLOR_CHANGE_DIRECT
+
+// reset blade color
+#define RESET_COLOR
+
+#endif
+
+#ifdef CONFIG_PROP
+#include "../props/saber_son_buttons.h"
+#endif
+
+#ifdef CONFIG_PRESETS
+#include "son_presets.h"
+
+/*
+Preset lengthFinder[] = {
+  {
+    "heroes/owk;heroes/common", "",
+    StylePtr<LengthFinder<>>(),
+    "numpixel",
+  },
+};
+*/
+
+Preset noBladePresets[] = {
+  noBladePreset,
+  owkPreset,
+  sonPreset,
+  reyPreset,
+  purplePreset,
+  ob4Preset,
+  kyloPreset,
+  yodaPreset,
+  darksaberPreset,
+  ezraPreset,
+  ahsokaPreset,
+  lukePreset,
+  vaderPreset,
+};
+
+Preset presets[] = {
+  owkPreset,
+  sonPreset,
+  reyPreset,
+  purplePreset,
+  ob4Preset,
+  kyloPreset,
+  yodaPreset,
+  darksaberPreset,
+  ezraPreset,
+  ahsokaPreset,
+  lukePreset,
+  vaderPreset,
+};
+
+BladeConfig blades[] = {
+  {
+    33,
+// 28" bendu blade
+    WS281XBladePtr<97, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+// npxl
+    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    CONFIGARRAY(presets),
+    "CONFIG/blade"
+  },
+  {
+    41,
+// 20" bendu blade
+    WS281XBladePtr<67, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+// npxl
+    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    CONFIGARRAY(presets),
+    "CONFIG/blade"
+  },
+  {
+    NO_BLADE,
+// 7" bendu blade
+    WS281XBladePtr<21, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+// npxl
+    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    CONFIGARRAY(noBladePresets),
+    "noblade",
+  },
+};
+#endif
+
+#ifdef CONFIG_BUTTONS
+Button PowerButton(BUTTON_POWER, powerButtonPin, "pow");
+#endif
+
