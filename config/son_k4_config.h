677594fe99fb86e3e1f831b0191ef33ade18db4e
Sonny Huynh <sonny@sonnyhuynh.com>
2020-10-08 16:21:01 -0400

    Add k4 config

---
+++ config/son_k4_config.h
@@ -0,0 +1,127 @@
+#ifdef CONFIG_TOP
+#include "proffieboard_v2_config.h"
+
+#define NUM_BLADES 3
+#define NUM_BUTTONS 2
+#define VOLUME 100
+#define MAX_VOLUME 1500
+const unsigned int maxLedsPerStrip = 144;
+#define CLASH_THRESHOLD_G 2.9
+#define ENABLE_AUDIO
+#define ENABLE_MOTION
+#define ENABLE_WS2811
+#define ENABLE_SD
+#define ENABLE_SSD1306
+#define SAVE_PRESET
+// this creates the preset.ini file
+#define SAVE_COLOR_CHANGE
+
+// npxl
+#define SHARED_POWER_PINS
+
+// blade detect
+#define BLADE_DETECT_PIN blade3Pin
+
+// blade id
+#define ENABLE_POWER_FOR_ID PowerPINS<bladePowerPin2,bladePowerPin3>
+
+// blade id pull up
+#define BLADE_ID_CLASS ExternalPullupBladeID<bladeIdentifyPin, 27000>
+
+#define SON_SWING_ON
+#define SON_TWIST_ON
+#define SON_TWIST_OFF
+#define FETT263_BATTLE_MODE
+
+// dual phase styles
+#define COLOR_CHANGE_DIRECT
+
+// reset blade color
+#define RESET_COLOR
+
+#endif
+
+#ifdef CONFIG_PROP
+#include "../props/saber_son_buttons.h"
+#endif
+
+#ifdef CONFIG_PRESETS
+#include "son_k4_presets.h"
+
+/*
+Preset lengthFinder[] = {
+  {
+    "heroes/owk;heroes/common", "",
+    StylePtr<LengthFinder<>>(),
+    "numpixel",
+  },
+};
+*/
+
+Preset noBladePresets[] = {
+  noBladePreset,
+  ob4Preset,
+  kyloPreset,
+  yodaPreset,
+  darksaberPreset,
+  ezraPreset,
+  ahsokaPreset,
+  lukePreset,
+  vaderPreset,
+  owkPreset,
+  sonPreset,
+  reyPreset,
+  purplePreset,
+};
+
+Preset presets[] = {
+  ob4Preset,
+  kyloPreset,
+  yodaPreset,
+  darksaberPreset,
+  ezraPreset,
+  ahsokaPreset,
+  lukePreset,
+  vaderPreset,
+  owkPreset,
+  sonPreset,
+  reyPreset,
+  purplePreset,
+};
+
+BladeConfig blades[] = {
+  {
+    0,
+    SubBlade(5, 101,
+    // 28" bendu blade, 97px + 5px emitter
+    WS281XBladePtr<102, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >()
+    ),
+    // npxl emitter, 5px
+    SubBlade(0,4, NULL),
+    // npxl chassis
+    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    CONFIGARRAY(presets),
+    "CONFIG/blade"
+  },
+  {
+    NO_BLADE,
+    SubBlade(1, 1,
+    // 28" bendu blade, 97px + 5px emitter
+    WS281XBladePtr<2, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >()
+    ),
+    // npxl emitter, 5px
+    SubBlade(0,0, NULL),
+    // npxl chassis
+    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    CONFIGARRAY(noBladePresets),
+    "CONFIG/noblade"
+  },
+};
+
+#endif
+
+#ifdef CONFIG_BUTTONS
+Button PowerButton(BUTTON_POWER, powerButtonPin, "pow");
+Button AuxButton(BUTTON_AUX, auxPin, "aux");
+#endif
+
---
+++ config/son_k4_presets.h
@@ -0,0 +1,107 @@
+#include "../blades/son_blades.h"
+
+Preset owkPreset = {
+  "son/owk;son/common;heroes/owk;heroes/common", "heroes/common/tracks/BattleOfHeros.wav",
+  owkStyle,
+  owkStyle,
+  owkStyle,
+  "OWK",
+};
+
+Preset sonPreset = {
+  "son/common;Rogue", "son/tracks/TheForce4.wav",
+  sonStyle,
+  sonStyle,
+  sonStyle,
+  "son",
+};
+
+Preset reyPreset = {
+  "son/rey;son/common;OB4", "heroes/common/tracks/BattleOfHeros.wav",
+  reyStyle,
+  reyStyle,
+  reyStyle,
+  "rey",
+};
+
+Preset purplePreset = {
+  "son/common;workshop/purple;The_Tragedy", "The_Tragedy/tracks/track1.wav",
+  purpleStyle,
+  purpleStyle,
+  purpleStyle,
+  "tragedy",
+};
+
+Preset ob4Preset = {
+  "son/common;OB4", "OB4/tracks/Force_Theme.wav",
+  ob4Style,
+  ob4Style,
+  ob4Style,
+  "ob4",
+};
+
+Preset kyloPreset = {
+  "son/kylo;son/common;Unstable_v2", "Unstable_v2/tracks/track4.wav",
+  kyloStyle,
+  kyloStyle,
+  kyloStyle,
+  "kylo",
+};
+
+Preset yodaPreset = {
+  "son/yoda;son/common;Little_Green_Friend", "Little_Green_Friend/tracks/YvsD.wav",
+  yodaStyle,
+  yodaStyle,
+  yodaStyle,
+  "yoda",
+};
+
+Preset darksaberPreset = {
+  "son/common;Dark_V2/Moff_Gideon;Dark_V2/Font", "Dark_V2/Font/tracks/track2.wav",
+  darksaberStyle,
+  darksaberStyle,
+  darksaberStyle,
+  "darksaber",
+};
+
+Preset ezraPreset = {
+  "son/ezra;son/common;Staple_Sword", "Staple_Sword/tracks/EzraTheme.wav",
+  ezraStyle,
+  ezraStyle,
+  ezraStyle,
+  "ezra",
+};
+
+Preset ahsokaPreset = {
+  "son/ahsoka;son/common;Code_Fulcrum", "Code_Fulcrum/tracks/track1.wav",
+  ahsokaStyle,
+  ahsokaStyle,
+  ahsokaStyle,
+  "ahsoka",
+};
+
+Preset lukePreset = {
+  "son/luke;son/common;ANH_Training", "ANH_Training/tracks/Training_Ambience.wav",
+  lukeStyle,
+  lukeStyle,
+  lukeStyle,
+  "luke",
+};
+
+Preset vaderPreset = {
+  "son/vader;son/common;Father", "Father/tracks/Imperial_March_Short.wav",
+  vaderStyle,
+  vaderStyle,
+  vaderStyle,
+  "vader",
+};
+
+Preset noBladePreset = {
+  "son/common;Calibrate", "heroes/common/tracks/BattleOfHeros.wav",
+  nobladeStyle,
+  nobladeStyle,
+  nobladeStyle,
+  "noblade",
+};
+
+
