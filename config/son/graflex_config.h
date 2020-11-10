73b04012a9ca05c4e8458230bebddb6ec0a7f0b9
Sonny Huynh <sonny@sonnyhuynh.com>
2020-11-09 19:42:35 -0500

    Extract blade id configuration

--- config/son/graflex_config.h
+++ config/son/graflex_config.h
@@ -85,62 +85,8 @@ Preset presets[] = {
   catPreset,
 };
 
-BladeConfig blades[] = {
-  {
-    0,
-    WS281XBladePtr<144, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    CONFIGARRAY(presets),
-    "CONFIG/blade"
-  },
-  {
-    5600,
-    // 28" bendu blade
-    WS281XBladePtr<97, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    CONFIGARRAY(presets),
-    "CONFIG/blade"
-  },
-  {
-    7400,
-    // 7" bendu blade
-    WS281XBladePtr<19, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    CONFIGARRAY(presets),
-    "CONFIG/blade"
-  },
-  {
-    8400,
-    // 20" bendu blade
-    WS281XBladePtr<67, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    CONFIGARRAY(presets),
-    "CONFIG/blade"
-  },
-  {
-    39000,
-    // 24" blade
-    WS281XBladePtr<83, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    CONFIGARRAY(presets),
-    "CONFIG/blade"
-  },
-  {
-    68000,
-    // 16" blade
-    WS281XBladePtr<54, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    CONFIGARRAY(presets),
-    "CONFIG/blade"
-  },
-  {
-    NO_BLADE,
-    WS281XBladePtr<1, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    CONFIGARRAY(noBladePresets),
-    "CONFIG/noblade",
-  },
-};
+#include "blade_config.h"
+
 #endif
 
 #ifdef CONFIG_BUTTONS
