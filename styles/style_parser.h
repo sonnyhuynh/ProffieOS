a07e8a23937b2ed1f63877139cf0e91733e8a143
Sonny Huynh <sonny@sonnyhuynh.com>
2020-11-12 19:09:05 -0500

    Remove default styles from style parser

--- styles/style_parser.h
+++ styles/style_parser.h
@@ -41,59 +41,6 @@ public:
 BuiltinPresetAllocator builtin_preset_allocator;
 
 NamedStyle named_styles[] = {
-  { "standard", StyleNormalPtrX<RgbArg<1, CYAN>, RgbArg<2, WHITE>, IntArg<3, 300>, IntArg<4, 800>>(),
-    "Standard blade, color, clash color, extension time, retraction time",
-  },
-  // Combine onspark, inoutsparktip, gradient, customizable blast/clash/lockup colors
-  { "advanced",
-    StylePtr<
-      InOutSparkTipX<
-        SimpleClash<
-          Lockup<
-            Blast<
-              OnSparkX<
-                Gradient<RgbArg<1, Red>, RgbArg<2, Blue>, RgbArg<3, Green>>,
-                RgbArg<4, White>,
-                IntArg<5, 10>
-              >,
-              RgbArg<6, White>, // blast color
-              200, 100, 400
-            >,
-            AudioFlicker<RgbArg<7, Magenta>, White>
-          >,
-          RgbArg<8, White>,
-          40
-        >,
-        InOutFuncX<IntArg<9, 300>, IntArg<10, 800> >,
-        RgbArg<11, White>
-      >
-    >(),
-    "Advanced blade, color at hilt, middle color, tip color, onspark color, onspark time, blast color, lockup color, clash color, extension time, retraction time, spark tip color",
-  },
-  { "fire",
-    StyleFirePtr<RgbArg<1, RED>, RgbArg<2, YELLOW>>(),
-    "Fire blade, warm color, hot color"
-  },
-  { "unstable",
-    StylePtr<InOutHelperX<LocalizedClash<Lockup<Blast<OnSpark<BrownNoiseFlicker<Strobe<RgbArg<1, Rgb<150, 0, 0>>,Sparkle<RgbArg<3, Rgb<255,40,0>>, RgbArg<4, Rgb<255,255,10>>,100,1024>,100,50>,Strobe<RgbArg<2, Red>,RgbArg<1, Rgb<150, 0, 0>>,50,5>,100>,White,100>,White,200,100,400>,AudioFlicker<OnSpark<BrownNoiseFlicker<Strobe<Black,Yellow,50,1>,Strobe<RgbArg<2, Red>,Black,50,1>,50>,White,200>,White>,AudioFlicker<OnSpark<BrownNoiseFlicker<Strobe<Black,Yellow,50,1>,Strobe<RgbArg<2, Red>,Black,50,1>,50>,White,200>,White>>,White,60,100>,InOutFuncX<IntArg<5, 100>,IntArg<6, 200>>,Black>>(),
-    "Unstable blade, warm, warmer, hot, sparks, extension time, retraction time"
-  },
-  { "strobe",
-    StyleNormalPtrX<StrobeX<RgbArg<1, BLACK>, RgbArg<2, WHITE>, IntArg<3, 15>, IntArg<4, 1>>, Rainbow, IntArg<5, 300>, IntArg<6, 800>>(),
-    "Stroboscope, standby color, flash color, flash frequency, flash milliseconds, extension time, retraction time"
-  },
-  { "cycle",
-    StylePtr<ColorCycle<RgbArg<1, Blue>,0,1,Layers<
-        AudioFlicker<RgbArg<3, Cyan>, RgbArg<2, Blue>>,
-        BlastL<RgbArg<4, Rgb<255,50,50>>>,
-        LockupL<HumpFlicker<RgbArg<5, Red>, RgbArg<3, Cyan>,100>>,
-        SimpleClashL<White>>,
-      100,2000,1000>>(),
-    "Cycle blade, start color, base color, flicker color, blast color, lockup color"
-  },
-  { "rainbow", StyleRainbowPtrX<IntArg<1, 300>, IntArg<2, 800>>(),
-    "Rainbow blade, extension time, retraction time"
-  },
   { "audioflicker",
     audioflickerArgStyle,
     "audioflicker blade, primary color, flicker color 1, flicker color 2, lockup start color, lockup color 1, lockup color 2, lockup end color, stab color, blast color, clash color, drag color, melt color 1, melt color 2",
@@ -102,7 +49,6 @@ NamedStyle named_styles[] = {
     supremeLeaderArgStyle,
     "supreme leader blade, primary color, flicker color 1, flicker color 2, lockup start color, lockup color 1, lockup color 2, lockup end color, stab color, blast color, clash color, drag color, melt color 1, melt color 2",
   },
-  { "charging", &style_charging, "Charging style" },
   { "builtin", &builtin_preset_allocator,
     "builtin preset styles, preset num, style index"
   },
