b463ec5f97190154205ad6bdd520421745cae8ec
Sonny Huynh <sonny@sonnyhuynh.com>
2020-11-27 19:53:10 -0500

    Remove duplicate presets

--- config/son/flagship_presets.h
+++ config/son/flagship_presets.h
@@ -14,24 +14,3 @@ Preset purplePreset = {
   "tragedy",
 };
 
-Preset darksaberPreset = {
-  "son/common;Dark_V2/Moff_Gideon;Dark_V2/Font", "Dark_V2/Font/tracks/track2.wav",
-  darksaberStyle,
-  darksaberStyle,
-  "darksaber",
-};
-
-Preset tanoPreset = {
-  "son/common;Commander_Tano", "Commander_Tano/tracks/track2.wav",
-  tanoStyle,
-  tanoStyle,
-  "tano",
-};
-
-Preset ahsokaPreset = {
-  "son/ahsoka;son/common;Code_Fulcrum", "Code_Fulcrum/tracks/track1.wav",
-  ahsokaStyle,
-  ahsokaStyle,
-  "ahsoka",
-};
-
