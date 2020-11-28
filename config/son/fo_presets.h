94bd43ca96f15f014a99e9d78d03c1a113c575c0
Sonny Huynh <sonny@sonnyhuynh.com>
2020-11-27 20:21:17 -0500

    Add fallen order blades and presets

---
+++ config/son/fo_presets.h
@@ -0,0 +1,65 @@
+#include "../../blades/son/fo_blades.h"
+
+Preset blueFoPreset = {
+  "son/common;The_Survivor_Bundle/Blue", "",
+  blueFoStyle,
+  blueFoStyle,
+  "fo blue",
+};
+
+Preset greenFoPreset = {
+  "son/common;The_Survivor_Bundle/Green", "",
+  greenFoStyle,
+  greenFoStyle,
+  "fo green",
+};
+
+Preset purpleFoPreset = {
+  "son/common;The_Survivor_Bundle/Purple", "",
+  purpleFoStyle,
+  purpleFoStyle,
+  "fo purple",
+};
+
+Preset orangeFoPreset = {
+  "son/common;The_Survivor_Bundle/Orange", "",
+  orangeFoStyle,
+  orangeFoStyle,
+  "fo orange",
+};
+
+Preset yellowFoPreset = {
+  "son/common;The_Survivor_Bundle/Yellow", "",
+  yellowFoStyle,
+  yellowFoStyle,
+  "fo yellow",
+};
+
+Preset cyanFoPreset = {
+  "son/common;The_Survivor_Bundle/Cyan", "",
+  cyanFoStyle,
+  cyanFoStyle,
+  "fo cyan",
+};
+
+Preset magentaFoPreset = {
+  "son/common;The_Survivor_Bundle/Magenta", "",
+  magentaFoStyle,
+  magentaFoStyle,
+  "fo magenta",
+};
+
+Preset indigoFoPreset = {
+  "son/common;The_Survivor_Bundle/Indigo", "",
+  indigoFoStyle,
+  indigoFoStyle,
+  "fo indigo",
+};
+
+Preset redFoPreset = {
+  "son/common;The_Survivor_Bundle/Red", "",
+  redFoStyle,
+  redFoStyle,
+  "fo red",
+};
+
