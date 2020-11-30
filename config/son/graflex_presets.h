c4874024b56e1b80cb78d59afa10ecd8a5562103
Sonny Huynh <sonny@sonnyhuynh.com>
2020-11-30 10:52:13 -0500

    Update canon blue blade styles

--- config/son/graflex_presets.h
+++ config/son/graflex_presets.h
@@ -1,30 +1,30 @@
 #include "../../blades/son/graflex_blades.h"
 
-Preset lukePreset = {
+Preset anhPreset = {
   "son/luke;son/common;ANH_Training", "ANH_Training/tracks/Training_Ambience.wav",
-  lukeStyle,
-  lukeStyle,
-  "luke",
+  graflexOTStyle,
+  graflexOTStyle,
+  "anh",
 };
 
 Preset esbPreset = {
   "son/common;ESB_Graflex", "ESB_Graflex/tracks/LvsV.wav",
-  lukeStyle,
-  lukeStyle,
+  graflexOTStyle,
+  graflexOTStyle,
   "esb",
 };
 
 Preset reyFlexPreset = {
   "son/common;ReFLEX/rey", "ReFLEX/tracks/Trailer2_Short.wav",
-  sequelStyle,
-  sequelStyle,
+  graflexSTStyle,
+  graflexSTStyle,
   "reyflex",
 };
 
 Preset benFlexPreset = {
   "son/common;ReFLEX/ben", "ReFLEX/tracks/Ben_vs_Knights.wav",
-  sequelStyle,
-  sequelStyle,
+  graflexSTStyle,
+  graflexSTStyle,
   "benflex",
 };
 
