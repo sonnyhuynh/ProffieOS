cdb020a1bcc906209e95cc6956e376bda62acbbd
Sonny Huynh <sonny@sonnyhuynh.com>
2020-11-30 11:09:19 -0500

    Add mw2 presets

---
+++ config/son/mw2_presets.h
@@ -0,0 +1,16 @@
+#include "../../blades/son/mw2_blades.h"
+
+Preset macePreset = {
+  "son/common;MACE", "",
+  maceStyle,
+  maceStyle,
+  "mace",
+};
+
+Preset powerRangersPreset = {
+  "son/common;MMPR", "",
+  powerRangersStyle,
+  powerRangersStyle,
+  "gogo",
+};
+
