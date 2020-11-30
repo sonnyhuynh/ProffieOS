f202a53d19fc63e6c1c6aad235525b1ee0ab6240
Sonny Huynh <sonny@sonnyhuynh.com>
2020-11-29 19:05:33 -0500

    Fix fo presets

--- config/son/fo_presets.h
+++ config/son/fo_presets.h
@@ -1,63 +1,63 @@
 #include "../../blades/son/fo_blades.h"
 
 Preset blueFoPreset = {
-  "son/common;The_Survivor_Bundle/Blue", "",
+  "son/common;The_Survivor_Bundle/common;The_Survivor_Bundle/Blue", "",
   blueFoStyle,
   blueFoStyle,
   "fo blue",
 };
 
 Preset greenFoPreset = {
-  "son/common;The_Survivor_Bundle/Green", "",
+  "son/common;The_Survivor_Bundle/common;The_Survivor_Bundle/Green", "",
   greenFoStyle,
   greenFoStyle,
   "fo green",
 };
 
 Preset purpleFoPreset = {
-  "son/common;The_Survivor_Bundle/Purple", "",
+  "son/common;The_Survivor_Bundle/common;The_Survivor_Bundle/Purple", "",
   purpleFoStyle,
   purpleFoStyle,
   "fo purple",
 };
 
 Preset orangeFoPreset = {
-  "son/common;The_Survivor_Bundle/Orange", "",
+  "son/common;The_Survivor_Bundle/common;The_Survivor_Bundle/Orange", "",
   orangeFoStyle,
   orangeFoStyle,
   "fo orange",
 };
 
 Preset yellowFoPreset = {
-  "son/common;The_Survivor_Bundle/Yellow", "",
+  "son/common;The_Survivor_Bundle/common;The_Survivor_Bundle/Yellow", "",
   yellowFoStyle,
   yellowFoStyle,
   "fo yellow",
 };
 
 Preset cyanFoPreset = {
-  "son/common;The_Survivor_Bundle/Cyan", "",
+  "son/common;The_Survivor_Bundle/common;The_Survivor_Bundle/Cyan", "",
   cyanFoStyle,
   cyanFoStyle,
   "fo cyan",
 };
 
 Preset magentaFoPreset = {
-  "son/common;The_Survivor_Bundle/Magenta", "",
+  "son/common;The_Survivor_Bundle/common;The_Survivor_Bundle/Magenta", "",
   magentaFoStyle,
   magentaFoStyle,
   "fo magenta",
 };
 
 Preset indigoFoPreset = {
-  "son/common;The_Survivor_Bundle/Indigo", "",
+  "son/common;The_Survivor_Bundle/common;The_Survivor_Bundle/Indigo", "",
   indigoFoStyle,
   indigoFoStyle,
   "fo indigo",
 };
 
 Preset redFoPreset = {
-  "son/common;The_Survivor_Bundle/Red", "",
+  "son/common;The_Survivor_Bundle/common;The_Survivor_Bundle/Red", "",
   redFoStyle,
   redFoStyle,
   "fo red",
