fc023126e82c0b3bd5ab9751ebd7ad3c60409ac5
Sonny Huynh <sonny@sonnyhuynh.com>
2020-12-07 19:12:25 -0500

    Update fun blades

--- blades/son/fun_blades.h
+++ blades/son/fun_blades.h
@@ -1,22 +1,28 @@
-// pikachu style
+// fully responsive audioflicker canon gold
+// mod to fire style
+// lockup effect color: blue mod
+// stab color blue mod
+// multi-blast 2
+// blast color: blue
+// clash color: blue mod
+// ignition 2000ms
 // retraction wavlen
 // on demand battery level: % blade - green to red (300ms ignition, 1s display, 500ms retraction mods)
+// power save all effects 25%
 StyleAllocator pikachuStyle =
 StylePtr<Layers<
   StyleFire<Yellow,Blue,0,3,FireConfig<0,2000,5>,FireConfig<0,2000,5>,FireConfig<0,2000,5>,FireConfig<0,2000,5>>,
-  TransitionEffectL<TrConcat<TrInstant,BrownNoiseFlickerL<AlphaL<White,Int<16000>>,Int<50>>,TrSmoothFade<600>>,EFFECT_LOCKUP_END>,
-  ResponsiveLockupL<Strobe<Blue,BrownNoiseFlicker<White,Blue,300>,50,1>,TrConcat<TrInstant,White,TrFade<400>>,TrFade<100>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>,Scale<SwingSpeed<100>,Int<10000>,Int<14000>>>,
+  LockupTrL<Layers<AlphaL<AudioFlickerL<Blue>,Bump<Scale<BladeAngle<>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>>,Scale<SwingSpeed<100>,Int<14000>,Int<18000>>>>,AlphaL<Blue,Bump<Scale<BladeAngle<>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>>,Int<10000>>>>,TrConcat<TrInstant,White,TrFade<400>>,TrConcat<TrInstant,White,TrFade<400>>,SaberBase::LOCKUP_NORMAL>,
   ResponsiveLightningBlockL<Strobe<White,AudioFlicker<White,Blue>,50,1>,TrConcat<TrInstant,AlphaL<White,Bump<Int<12000>,Int<18000>>>,TrFade<200>>,TrConcat<TrInstant,HumpFlickerL<AlphaL<White,Int<16000>>,30>,TrSmoothFade<600>>>,
-  ResponsiveStabL<Blue>,
+  ResponsiveStabL<Blue,TrWipeIn<600>,TrWipe<600>>,
   EffectSequence<EFFECT_BLAST,
+    TransitionEffectL<TrConcat<TrInstant,AlphaL<Blue,BlastF<200,200>>,TrFade<300>>,EFFECT_BLAST>,
     ResponsiveBlastL<Blue,Int<400>,Scale<SwingSpeed<200>,Int<100>,Int<400>>,Int<400>>,
-    LocalizedClashL<Blue,80,30,EFFECT_BLAST>,
     ResponsiveBlastWaveL<Blue,Scale<SwingSpeed<400>,Int<500>,Int<200>>,Scale<SwingSpeed<400>,Int<100>,Int<400>>>,
-    BlastL<Blue,200,200>,
     ResponsiveBlastFadeL<Blue,Scale<SwingSpeed<400>,Int<6000>,Int<12000>>,Scale<SwingSpeed<400>,Int<400>,Int<100>>>,
     ResponsiveBlastL<Blue,Scale<SwingSpeed<400>,Int<400>,Int<100>>,Scale<SwingSpeed<400>,Int<200>,Int<100>>,Scale<SwingSpeed<400>,Int<400>,Int<200>>>
   >,
-  ResponsiveClashL<Blue,TrInstant,TrFade<400>>,
+  ResponsiveClashL<Blue,TrInstant,TrFade<400>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>,Int<20000>>,
   LockupTrL<AlphaL<BrownNoiseFlickerL<White,Int<300>>,SmoothStep<Int<30000>,Int<5000>>>,TrWipeIn<400>,TrFade<300>,SaberBase::LOCKUP_DRAG>,
   LockupTrL<AlphaL<Mix<TwistAngle<>,Red,Orange>,SmoothStep<Int<28000>,Int<5000>>>,TrWipeIn<600>,TrFade<300>,SaberBase::LOCKUP_MELT>,
   EffectSequence<EFFECT_POWERSAVE,AlphaL<Black,Int<8192>>,AlphaL<Black,Int<16384>>,AlphaL<Black,Int<24576>>,AlphaL<Black,Int<0>>>,
@@ -25,30 +31,11 @@ StylePtr<Layers<
 >>()
 ;
 
-// TODO
-// smash style from kyberphonics
+// fully responsive kaleidoscope full spectrum
+// multi-blast 2
+// on demand battery level: % blade - green to red (300ms ignition, 1s display, 500ms retraction mods)
+// power save all effects 25%
 StyleAllocator smashStyle =
-StylePtr<
-  InOutHelper<
-    SimpleClash<
-      Lockup<
-        Blast<
-          Gradient<Gradient<Cyan,Blue,Magenta>,Gradient<Red,Red,OrangeRed>>,
-          White,
-          200,
-          100,
-          400
-        >,
-        AudioFlicker<Gradient<Red,Blue>,White>,
-        AudioFlicker<Gradient<Red,Blue>,White>
-      >,
-      Gradient<Cyan,Yellow>,
-      40
-    >,
-    300,
-    800,
-    Black
-  >
->()
+StylePtr<Layers<Mix<SmoothStep<Int<16384>,Int<0>>,Mix<SmoothStep<Int<8192>,Int<0>>,StripesX<Int<3000>,Scale<TwistAngle<1>,Int<-250>,Int<250>>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Red>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Rgb<60,0,0>>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Rgb<128,0,0>>>,StripesX<Int<3000>,Scale<TwistAngle<1>,Int<250>,Int<-250>>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Red>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Rgb<60,0,0>>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Rgb<128,0,0>>>>,Mix<SmoothStep<Int<24576>,Int<0>>,StripesX<Int<3000>,Scale<TwistAngle<1>,Int<-250>,Int<250>>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Red>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Rgb<60,0,0>>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Rgb<128,0,0>>>,StripesX<Int<3000>,Scale<TwistAngle<1>,Int<250>,Int<-250>>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Red>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Rgb<60,0,0>>,RotateColorsX<Sin<Scale<TwistAngle<1>,Int<6>,Int<0>>>,Rgb<128,0,0>>>>>,LockupTrL<Layers<AlphaL<AudioFlickerL<White>,Bump<Scale<BladeAngle<>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>>,Scale<SwingSpeed<100>,Int<14000>,Int<18000>>>>,AlphaL<White,Bump<Scale<BladeAngle<>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>>,Int<10000>>>>,TrConcat<TrInstant,White,TrFade<400>>,TrConcat<TrInstant,White,TrFade<400>>,SaberBase::LOCKUP_NORMAL>,ResponsiveLightningBlockL<Strobe<White,AudioFlicker<White,Blue>,50,1>,TrConcat<TrInstant,AlphaL<White,Bump<Int<12000>,Int<18000>>>,TrFade<200>>,TrConcat<TrInstant,HumpFlickerL<AlphaL<White,Int<16000>>,30>,TrSmoothFade<600>>>,ResponsiveStabL<Orange,TrWipeIn<600>,TrWipe<600>>,EffectSequence<EFFECT_BLAST,TransitionEffectL<TrConcat<TrInstant,AlphaL<White,BlastF<200,200>>,TrFade<300>>,EFFECT_BLAST>,ResponsiveBlastL<White,Int<400>,Scale<SwingSpeed<200>,Int<100>,Int<400>>,Int<400>>,ResponsiveBlastWaveL<White,Scale<SwingSpeed<400>,Int<500>,Int<200>>,Scale<SwingSpeed<400>,Int<100>,Int<400>>>,ResponsiveBlastFadeL<White,Scale<SwingSpeed<400>,Int<6000>,Int<12000>>,Scale<SwingSpeed<400>,Int<400>,Int<100>>>,ResponsiveBlastL<White,Scale<SwingSpeed<400>,Int<400>,Int<100>>,Scale<SwingSpeed<400>,Int<200>,Int<100>>,Scale<SwingSpeed<400>,Int<400>,Int<200>>>>,ResponsiveClashL<White,TrInstant,TrFade<400>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>,Int<20000>>,LockupTrL<AlphaL<BrownNoiseFlickerL<White,Int<300>>,SmoothStep<Int<30000>,Int<5000>>>,TrWipeIn<400>,TrFade<300>,SaberBase::LOCKUP_DRAG>,LockupTrL<AlphaL<Mix<TwistAngle<>,Rgb<255,200,0>,DarkOrange>,SmoothStep<Int<28000>,Int<5000>>>,TrWipeIn<600>,TrFade<300>,SaberBase::LOCKUP_MELT>,EffectSequence<EFFECT_POWERSAVE,AlphaL<Black,Int<8192>>,AlphaL<Black,Int<16384>>,AlphaL<Black,Int<24576>>,AlphaL<Black,Int<0>>>,InOutTrL<TrWipe<300>,TrWipeIn<500>,Black>,TransitionEffectL<TrConcat<TrWipe<1000>,AlphaL<Mix<BatteryLevel,Red,Green>,SmoothStep<BatteryLevel,Int<-10>>>,TrDelay<2000>,AlphaL<Mix<BatteryLevel,Red,Green>,SmoothStep<BatteryLevel,Int<-10>>>,TrWipeIn<1000>>,EFFECT_BATTERY_LEVEL>>>()
 ;
 
