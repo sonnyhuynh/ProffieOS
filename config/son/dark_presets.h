2444a38038e0ace7b0db27fd72ca85ad4a9580f2
Sonny Huynh <sonny@sonnyhuynh.com>
2021-01-07 19:39:58 -0500

    Change purple preset to palpatine preset

--- config/son/dark_presets.h
+++ config/son/dark_presets.h
@@ -7,10 +7,10 @@ Preset malakPreset = {
   "malak",
 };
 
-Preset purplePreset = {
-  "son/common;workshop/purple;The_Tragedy", "The_Tragedy/tracks/track1.wav",
-  purpleStyle,
-  purpleStyle,
+Preset palpatinePreset = {
+  "son/common;The_Tragedy", "The_Tragedy/tracks/track1.wav",
+  palpatineStyle,
+  palpatineStyle,
   "tragedy",
 };
 
