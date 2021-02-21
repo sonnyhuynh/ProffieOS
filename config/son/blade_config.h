dee3a2f8ad3b3cf0161d5713fbff8a5bc34c90be
Sonny Huynh <sonny@sonnyhuynh.com>
2021-02-21 11:28:27 -0500

    Extract blades from blade config

--- config/son/blade_config.h
+++ config/son/blade_config.h
@@ -1,71 +1,66 @@
+#include "blades.h"
+
 BladeConfig blades[] = {
   {
     0,
-    WS281XBladePtr<144, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<0, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    bladeMax,
+    npxlOff,
     CONFIGARRAY(presets),
     "CONFIG/blade"
   },
   {
     5600,
-    // 28" bendu blade
-    WS281XBladePtr<97, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<0, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    benduBlade28,
+    npxlOff,
     CONFIGARRAY(presets),
     "CONFIG/blade"
   },
   {
     7400,
-    // 7" bendu blade
-    WS281XBladePtr<19, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<0, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    benduBlade7,
+    npxlOff,
     CONFIGARRAY(presets),
     "CONFIG/blade"
   },
   {
     8400,
-    // 20" bendu blade
-    WS281XBladePtr<67, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<0, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    benduBlade20,
+    npxlOff,
     CONFIGARRAY(presets),
     "CONFIG/blade"
   },
   {
     33000,
-    // 28" blade
-    WS281XBladePtr<97, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<0, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    blade28,
+    npxlOff,
     CONFIGARRAY(presets),
     "CONFIG/blade"
   },
   {
     39000,
-    // 24" blade
-    WS281XBladePtr<83, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<0, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    blade24,
+    npxlOff,
     CONFIGARRAY(presets),
     "CONFIG/blade"
   },
   {
     68000,
-    // 16" blade
-    WS281XBladePtr<54, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<0, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    blade16,
+    npxlOff,
     CONFIGARRAY(presets),
     "CONFIG/blade"
   },
   {
     800000,
-    // 28" vv blade
-    WS281XBladePtr<100, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<0, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    vvBlade28,
+    npxlOff,
     CONFIGARRAY(presets),
     "CONFIG/blade"
   },
   {
     NO_BLADE,
-    WS281XBladePtr<0, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-    WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
+    bladeOff,
+    npxl,
     CONFIGARRAY(noBladePresets),
     "CONFIG/noblade",
   },
---
+++ config/son/blades.h
@@ -0,0 +1,17 @@
+BladeBase* bladeMax = WS281XBladePtr<144, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+
+BladeBase* vvBlade28 = WS281XBladePtr<100, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+
+BladeBase* benduBlade28 = WS281XBladePtr<97, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+BladeBase* benduBlade20 = WS281XBladePtr<67, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+BladeBase* benduBlade7 = WS281XBladePtr<19, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+
+BladeBase* blade28 = WS281XBladePtr<97, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+BladeBase* blade24 = WS281XBladePtr<83, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+BladeBase* blade16 = WS281XBladePtr<54, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+
+BladeBase* bladeOff = WS281XBladePtr<0, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+
+BladeBase* npxl = WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+BladeBase* npxlOff = WS281XBladePtr<0, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+
