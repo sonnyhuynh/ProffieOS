59eff3583bfd1d41cf17963c8bd5e296a7f8b8f2
Sonny Huynh <sonny@sonnyhuynh.com>
2021-04-01 19:04:40 -0400

    Add 12" blade

--- config/son/blades.h
+++ config/son/blades.h
@@ -9,6 +9,7 @@ BladeBase* benduBlade7 = WS281XBladePtr<19, bladePin, Color8::GRB, PowerPINS<bla
 BladeBase* blade28 = WS281XBladePtr<97, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
 BladeBase* blade24 = WS281XBladePtr<83, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
 BladeBase* blade16 = WS281XBladePtr<54, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
+BladeBase* blade12 = WS281XBladePtr<39, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
 
 BladeBase* bladeNpxl = WS281XBladePtr<5, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
 BladeBase* bladeOff = WS281XBladePtr<0, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >();
