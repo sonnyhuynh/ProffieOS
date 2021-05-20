91fe5b08ae05d5c3e70797d1540c45aa5431e958
Sonny Huynh <sonny@sonnyhuynh.com>
2021-05-20 19:41:38 -0400

    Adjust bors to 16 blade

--- config/son/bors_config.h
+++ config/son/bors_config.h
@@ -87,7 +87,7 @@ Preset presets[] = {
 BladeConfig blades[] = {
   {
     0,
-    blade24,
+    blade16,
     // chassis leds
     WS281XBladePtr<8, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin4> >(),
     // yellow button
